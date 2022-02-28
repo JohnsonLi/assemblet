ed2bd13e1542018889abb3cd21d4315f4a3f6cf4
Johnson Li <johnson.li61001@gmail.com>
2022-02-28 01:27:18 -0500

    added add and sub. sample fib.

--- util/interpreter.py
+++ util/interpreter.py
@@ -1,10 +1,11 @@
+from operator import le
 import ply.lex as lex
 
 tokens = ("LABEL", "INSTRUCTION", "NUMBER", "REGISTER")
 
 # Tokens
 t_LABEL = r"\w+:"
-t_INSTRUCTION = r"\s*(IN|OUT|ADD|MOVE|SUBSTRACT|MULTIPLY|DIVIDE|JUMP|JNE)"
+t_INSTRUCTION = r"\s*(IN|OUT|MOVE|ADD|SUB|MULT|DIV|JUMP|JNE)"
 t_REGISTER = r"\s*(A|B|C|D|E|F|G|H|X|Y|Z)"
 t_ignore = " \t"
 
@@ -26,7 +27,7 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
-with open("sample.mrtl") as f:
+with open("fib.mrtl") as f:
     data = f.read()
 
 program = []
@@ -59,7 +60,7 @@ class Interpreter:
 
     def __init__(self, program):
         self.program = program
-        self.registers = [0] * 6
+        self.registers = [0] * len(Register)
         self.pc = 0
 
     def handle_label(self):
@@ -77,15 +78,18 @@ class Interpreter:
         if line[0] == "JUMP":
             self._handle_jump(line[1:])
 
-
+        if line[0] == "ADD":
+            self._handle_add(line[1:])
         
+        if line[0] == "SUB":
+            self._handle_sub(line[1:])
+
         # Increment PC unless jump
         if line[0] != "JUMP":
             self.pc += 1
 
     def _handle_move(self, args):
         # syntax is : MOVE reg|num reg
-        
         if isinstance(args[0], int):
             # immediate to register
             self.registers[Register[args[1]].value] = args[0]
@@ -95,13 +99,29 @@ class Interpreter:
          
     def _handle_out(self, args):
         # syntax is : OUT reg
-
         print(self.registers[Register[args[0]].value])
 
     def _handle_jump(self, args):
         # syntax is : JUMP linenum
-
         self.pc = args[0] - 1 # -1 because index starts at 0
+
+    def _handle_add(self, args):
+        # syntax is : ADD reg|num reg
+        if isinstance(args[0], int):
+            # add immediate to register
+            self.registers[Register[args[1]].value] += args[0]
+        else:
+            # add register to register
+            self.registers[Register[args[1]].value] += self.registers[Register[args[0]].value]
+
+    def _handle_sub(self, args):
+        # syntax is : SUB reg|num reg
+        if isinstance(args[0], int):
+            # sub immediate from register
+            self.registers[Register[args[1]].value] -= args[0]
+        else:
+            # sub register from register
+            self.registers[Register[args[1]].value] -= self.registers[Register[args[0]].value]
 
     def execute(self):
         while self.pc < len(program):
@@ -123,5 +143,5 @@ class Interpreter:
 # ============================================================================= #
 
 a = Interpreter(program)
-print(a)
+print(program)
 a.execute()
