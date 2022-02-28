58ea526246a7b0552ca7577198ff33ad339bdde7
jiayang <jc9555@nyu.edu>
2022-02-28 13:03:14 -0500

    he said make it flash! make it flash!

--- static/js/assembly-builder.js
+++ static/js/assembly-builder.js
@@ -1,85 +1,58 @@
 // build the assembly for the interpreter
-
+const ASSEMBLY_BUILDER = document.getElementById("assembly-builder");
+const INSTRUCTION_LIST = document.getElementById("instruction-list");
 
 var init = function() {
     const NUM_INSTRUCTIONS = 10;
-
+    ASSEMBLY_BUILDER.instructions = [];
     // Setup the assembly builder where the user will create their script
-    const ASSEMBLY_BUILDER = document.getElementById("assembly-builder");
+    
     for (let i = 0; i < NUM_INSTRUCTIONS; i++) {
         let instructionEntry = document.createElement("div");
         instructionEntry.classList.add("instruction-entry");
         ASSEMBLY_BUILDER.appendChild(instructionEntry);
-
-        let name = document.createElement("span");
-        name.classList.add("unselectable");
-        instructionEntry.appendChild(name);
-        instructionEntry.nameSpan = name;
-
+        ASSEMBLY_BUILDER.instructions.push(instructionEntry);
         instructionEntry.onmouseup = function(event) {
-            if (window.moving) {
-                instructionEntry.nameSpan.innerHTML = window.movingInstruction.name;
-            }
-        }
-    }
-
-    var move = function(div, x, y) {
-        div.x = x;
-        div.y = y;
-        div.style.left = div.x + "px";
-        div.style.top = div.y + "px";
-    }
-
-    var setupInstruction = function(instructionName) {
-        let instruction = document.createElement("div");
-        instruction.classList.add("instruction");
-        instruction.name = instructionName;
-        instruction.x = 0;
-        instruction.y = 0;
-
-        let name = document.createElement("span");
-        name.classList.add("unselectable");
-        name.innerHTML = instructionName;
-        instruction.appendChild(name);
-
-        instruction.onmousedown = function(event) {
-            event.currentTarget.moving = true;
-            event.currentTarget.classList.add("instruction-moving");
-            event.currentTarget.clickX = event.clientX;
-            event.currentTarget.clickY = event.clientY;
-            window.moving = true;
-            window.movingInstruction = event.currentTarget;
-        };
-
-        window.onmousemove = function(event) {
-            if (window.moving) {
-                let newX = event.clientX - window.movingInstruction.clickX;
-                let newY = event.clientY - window.movingInstruction.clickY;
-                move(window.movingInstruction, newX, newY);
+            if (window.moving && event.currentTarget.children.length == 0) {
+                window.movingInstruction.parentElement.removeChild(window.movingInstruction);
+                event.currentTarget.appendChild(window.movingInstruction);
             }
         }
 
-        window.onmouseup = function(event) {
-            window.movingInstruction.moving = false;
-            window.moving = false;
-            move(window.movingInstruction, 0, 0);
-            window.movingInstruction.classList.remove("instruction-moving");
-            delete window.movingInstruction;
-        }
-
-
-
-        return instruction;
+        
     }
-
 
     // Setup the instruction list where the user will take instructions to put in their script
-    const INSTRUCTION_LIST = document.getElementById("instruction-list");
-    INSTRUCTIONS.forEach((instructionName) => {
+    
+    Object.keys(INSTRUCTIONS).forEach((instructionName) => {
         INSTRUCTION_LIST.appendChild(setupInstruction(instructionName));
-
     });
-
 }
+
+var compile = function() {
+    let compiled = [];
+    ASSEMBLY_BUILDER.instructions.forEach((instructionEntry) => {
+        if (instructionEntry.children.length != 0) {
+            let instruction = instructionEntry.children[0];
+            let s = instruction.name;
+            instruction.parameters.forEach((parameter) => {
+                if (parameter.value == "") {
+                    $(instruction).css({"border-color": "red", "border-width": "2px"});
+                    setTimeout(() => {
+                        $(instruction).css({"border-color": "black", "border-width": "1px"})
+                    }, 500);
+                }    
+                s += " " + parameter.value;
+            });
+            compiled.push(s);
+        }
+    });
+    console.log(compiled);
+}
+
+
+
+
+
 
 init();
--- static/js/instructions.js
+++ static/js/instructions.js
@@ -1,1 +1,64 @@
-const INSTRUCTIONS = ['mov', 'add'];
+//Instruction Name: num params
+const INSTRUCTIONS = {
+    'mov': 2, 
+    'add': 2
+};
+
+var setupInstruction = function(instructionName) {
+    let instruction = document.createElement("div");
+    instruction.classList.add("instruction", "unselectable");
+    instruction.name = instructionName;
+
+    let name = document.createElement("span");
+    name.classList.add("unselectable");
+    name.innerHTML = instructionName;
+    instruction.appendChild(name);
+
+    makeParameters(instruction, instructionName);
+
+    var move = function(div, x, y) {
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+    }
+
+    instruction.onmousedown = function(event) {
+        event.currentTarget.moving = true;
+        event.currentTarget.classList.add("instruction-moving");
+        event.currentTarget.clickX = event.clientX;
+        event.currentTarget.clickY = event.clientY;
+        window.moving = true;
+        window.movingInstruction = event.currentTarget;
+    };
+
+    window.onmousemove = function(event) {
+        if (window.moving) {
+            let newX = event.clientX - window.movingInstruction.clickX;
+            let newY = event.clientY - window.movingInstruction.clickY;
+            move(window.movingInstruction, newX, newY);
+        }
+    }
+
+    window.onmouseup = function(event) {
+        if (window.moving) {
+            window.movingInstruction.moving = false;
+            window.moving = false;
+            move(window.movingInstruction, 0, 0);
+            window.movingInstruction.classList.remove("instruction-moving");
+            delete window.movingInstruction;
+        }
+    }
+
+
+
+    return instruction;
+}
+
+var makeParameters = function(div, name) {
+    div.parameters = [];
+    for (let i = 0; i < INSTRUCTIONS[name]; i++) {
+        let input = document.createElement("input");
+        input.classList.add("instruction-parameter");
+        div.appendChild(input);
+        div.parameters.push(input);
+    }
+}
