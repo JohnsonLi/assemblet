e9416098ea96bcbce474e75b87a42a0e95b61886
Johnson Li <johnson.li61001@gmail.com>
2022-02-28 00:54:19 -0500

    rename goto to jump, added more instructions and registers to lexer

--- util/interpreter.py
+++ util/interpreter.py
@@ -4,8 +4,8 @@ tokens = ("LABEL", "INSTRUCTION", "NUMBER", "REGISTER")
 
 # Tokens
 t_LABEL = r"\w+:"
-t_INSTRUCTION = r"\s*(IN|OUT|ADD|MOVE|SUBSTRACT|MULTIPLY|DIVIDE|GOTO)"
-t_REGISTER = r"\s*(A|B|C|D|E|F)"
+t_INSTRUCTION = r"\s*(IN|OUT|ADD|MOVE|SUBSTRACT|MULTIPLY|DIVIDE|JUMP|JNE)"
+t_REGISTER = r"\s*(A|B|C|D|E|F|G|H|X|Y|Z)"
 t_ignore = " \t"
 
 def t_NUMBER(t):
@@ -49,6 +49,11 @@ class Register(Enum):
     D = 3
     E = 4
     F = 5
+    G = 6
+    H = 7
+    X = 8
+    Y = 9
+    Z = 10
 
 class Interpreter:
 
@@ -58,7 +63,7 @@ class Interpreter:
         self.pc = 0
 
     def handle_label(self):
-        pass
+        self.pc += 1
 
     def handle_instruction(self, line):
         # line: [Instruction, arg1, arg2]
@@ -69,17 +74,34 @@ class Interpreter:
         if line[0] == "OUT":
             self._handle_out(line[1:])
 
+        if line[0] == "JUMP":
+            self._handle_JUMP(line[1:])
+
+
+        
+        # Increment PC unless jump
+        if line[0] != "JUMP":
+            self.pc += 1
+
     def _handle_move(self, args):
         # syntax is : MOVE reg|num reg
         
-        # immediate to register
         if isinstance(args[0], int):
+            # immediate to register
             self.registers[Register[args[1]].value] = args[0]
-    
+        else:
+            # register to register
+            self.registers[Register[args[1]].value] = self.registers[Register[args[0]].value]
+         
     def _handle_out(self, args):
         # syntax is : OUT reg
 
         print(self.registers[Register[args[0]].value])
+
+    def _handle_JUMP(self, args):
+        # syntax is : JUMP linenum
+
+        self.pc = args[0] - 1 # -1 because index starts at 0
 
     def execute(self):
         while self.pc < len(program):
@@ -90,8 +112,6 @@ class Interpreter:
             
             elif line[0] == "INSTRUCTION":
                 self.handle_instruction(line[1:])
-            
-            self.pc += 1
 
     def __repr__(self):
         return f"{self.program}\n registers={self.registers}\n pc={self.pc}"
@@ -99,4 +119,5 @@ class Interpreter:
 # ============================================================================= #
 
 a = Interpreter(program)
+print(a)
 a.execute()
