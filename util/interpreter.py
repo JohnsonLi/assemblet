10d50a2b092565875ce0d7494fbf1fe19d235fe7
Johnson Li <johnson.li61001@gmail.com>
2022-02-28 00:35:29 -0500

    added basic interpreter. move and out

--- util/interpreter.py
+++ util/interpreter.py
@@ -1,5 +1,4 @@
 import ply.lex as lex
-import ply.yacc as yacc
 
 tokens = ("LABEL", "INSTRUCTION", "NUMBER", "REGISTER")
 
@@ -30,6 +29,74 @@ lexer = lex.lex()
 with open("sample.mrtl") as f:
     data = f.read()
 
+program = []
+
 lexer.input(data)
-for tok in lexer:
-    print(tok)
+for token in lexer:
+    if token.lineno <= len(program):
+        program[token.lineno - 1].append(token.value)
+    else:
+        program.append([token.type, token.value])
+
+# ============================================================================= #
+
+from enum import Enum
+
+class Register(Enum):
+    A = 0
+    B = 1
+    C = 2
+    D = 3
+    E = 4
+    F = 5
+
+class Interpreter:
+
+    def __init__(self, program):
+        self.program = program
+        self.registers = [0] * 6
+        self.pc = 0
+
+    def handle_label(self):
+        pass
+
+    def handle_instruction(self, line):
+        # line: [Instruction, arg1, arg2]
+
+        if line[0] == "MOVE":
+            self._handle_move(line[1:])
+
+        if line[0] == "OUT":
+            self._handle_out(line[1:])
+
+    def _handle_move(self, args):
+        # syntax is : MOVE reg|num reg
+        
+        # immediate to register
+        if isinstance(args[0], int):
+            self.registers[Register[args[1]].value] = args[0]
+    
+    def _handle_out(self, args):
+        # syntax is : OUT reg
+
+        print(self.registers[Register[args[0]].value])
+
+    def execute(self):
+        while self.pc < len(program):
+            line = program[self.pc]
+
+            if line[0] == "LABEL":
+                self.handle_label()
+            
+            elif line[0] == "INSTRUCTION":
+                self.handle_instruction(line[1:])
+            
+            self.pc += 1
+
+    def __repr__(self):
+        return f"{self.program}\n registers={self.registers}\n pc={self.pc}"
+
+# ============================================================================= #
+
+a = Interpreter(program)
+a.execute()
